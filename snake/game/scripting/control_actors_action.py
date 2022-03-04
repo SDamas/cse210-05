f3fb25b6b33b50cfb09c7907b81c0a37b296964b
Samad <samad.damas@hotmail.com>
2022-03-03 21:39:24 -0300

    Push code to start cycle game

---
+++ snake/game/scripting/control_actors_action.py
@@ -0,0 +1,79 @@
+import constants
+from game.scripting.action import Action
+from game.shared.point import Point
+
+
+class ControlActorsAction(Action):
+    """
+    An input action that controls the snake.
+    
+    The responsibility of ControlActorsAction is to get the direction and move the snake's head.
+
+    Attributes:
+        _keyboard_service (KeyboardService): An instance of KeyboardService.
+    """
+
+    def __init__(self, keyboard_service):
+        """Constructs a new ControlActorsAction using the specified KeyboardService.
+        
+        Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
+        """
+        self._keyboard_service = keyboard_service
+        self._direction_player_1 = Point(constants.CELL_SIZE, 0)
+        self._direction_player_2 = Point(constants.CELL_SIZE, 0)
+
+    def execute(self, cast, script):
+        """Executes the control actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+
+        player_1_direction = ["a", "d", "w", "s"]
+        player_2_direction = ["j", "l", "i", "k"]
+
+        # left
+        if self._keyboard_service.is_key_down('a'):
+            self._direction_player_1 = Point(-constants.CELL_SIZE, 0)
+        
+        # right
+        if self._keyboard_service.is_key_down('d'):
+            self._direction_player_1 = Point(constants.CELL_SIZE, 0)
+        
+        # up
+        if self._keyboard_service.is_key_down('w'):
+            self._direction_player_1 = Point(0, -constants.CELL_SIZE)
+        
+        # down
+        if self._keyboard_service.is_key_down('s'):
+            self._direction_player_1 = Point(0, constants.CELL_SIZE)
+
+        # left
+        if self._keyboard_service.is_key_down('j'):
+            self._direction_player_2 = Point(-constants.CELL_SIZE, 0)
+        
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._direction_player_2 = Point(constants.CELL_SIZE, 0)
+        
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._direction_player_2 = Point(0, -constants.CELL_SIZE)
+
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._direction_player_2 = Point(0, constants.CELL_SIZE)
+
+        snakes = cast.get_actors("snakes")
+        player_1 = snakes[0]
+        player_2 = snakes[1]
+
+        for key in player_1_direction:
+            if self._keyboard_service.is_key_down(key):
+                player_1.turn_head(self._direction_player_1)
+
+        for key in player_2_direction:
+            if self._keyboard_service.is_key_down(key):
+                player_2.turn_head(self._direction_player_2)
---
+++ snake/game/scripting/draw_actors_action.py
@@ -0,0 +1,44 @@
+from game.scripting.action import Action
+from game.casting.actor import Actor
+
+
+class DrawActorsAction(Action):
+    """
+    An output action that draws all the actors.
+    
+    The responsibility of DrawActorsAction is to draw all the actors.
+
+    Attributes:
+        _video_service (VideoService): An instance of VideoService.
+    """
+
+    def __init__(self, video_service):
+        """Constructs a new DrawActorsAction using the specified VideoService.
+        
+        Args:
+            video_service (VideoService): An instance of VideoService.
+        """
+        self._video_service = video_service
+
+    def execute(self, cast, script):
+        """Executes the draw actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        scores = cast.get_actors("scores") 
+        food = cast.get_first_actor("foods")
+        messages = cast.get_actors("messages")
+        snakes = cast.get_actors("snakes")
+
+        for score in scores:
+            self._video_service.draw_actor(score)
+        for snake in snakes:
+            segments = snake.get_segments()
+            self._video_service.draw_actors(segments)
+
+        self._video_service.clear_buffer()
+        self._video_service.draw_actor(food)
+        self._video_service.draw_actors(messages, True)
+        self._video_service.flush_buffer()
